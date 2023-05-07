7a75a312314a751b285e04cf3bbfb355ea610d0c
Nikita Starostin <starostin.450503@gmail.com>
2023-05-07 17:45:34 +0300

    add first functions

---
+++ api/azureFunctions/src/functions/test.ts
@@ -0,0 +1,15 @@
+import { app, HttpRequest, HttpResponse, InvocationContext } from "@azure/functions";
+
+export async function test(context: InvocationContext, request: HttpRequest): Promise<HttpResponse> {
+    context.log(`Http function processed request for url "${request.url}"`);
+
+    const name = request.query.get('name') || await request.text() || 'world';
+
+    return { body: `Hello, ${name}!` };
+};
+
+app.http('test', {
+    methods: ['GET'],
+    authLevel: 'anonymous',
+    handler: test
+});
