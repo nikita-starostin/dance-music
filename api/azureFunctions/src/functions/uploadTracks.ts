3be4be473e1124f3aed7f56db14f1beaf98de6e5
Nikita Starostin <starostin.450503@gmail.com>
2023-05-15 20:21:46 +0300

    add upload tracks function

---
+++ api/azureFunctions/src/functions/uploadTracks.ts
@@ -0,0 +1,32 @@
+﻿import {app, HttpRequest, HttpResponse, InvocationContext} from "@azure/functions";
+import {StorageSharedKeyCredential, BlobServiceClient} from "@azure/storage-blob";
+
+app.http('uploadTracks', {
+    methods: ['PUT'],
+    authLevel: 'anonymous',
+    handler: async (context: InvocationContext, request: HttpRequest): Promise<HttpResponse> => {
+        const formData = await request.formData();
+        const files = formData.getAll('tracks') as unknown as File[];
+
+        const storageAccountName = process.env.TracksStorageAccountName;
+        const storageAccountKey = process.env.TracksStorageAccountKey;
+        const containerName = process.env.TracksContainerName;
+        const sharedKeyCredential = new StorageSharedKeyCredential(storageAccountName, storageAccountKey);
+        const blobServiceClient = new BlobServiceClient(
+            `https://${storageAccountName}.blob.core.windows.net`,
+            sharedKeyCredential
+        );
+        const containerClient = blobServiceClient.getContainerClient(containerName);
+
+        for (const file of files) {
+            const blockBlobClient = containerClient.getBlockBlobClient(file.name);
+            await blockBlobClient.uploadData(await file.arrayBuffer())
+        }
+
+        return {
+            body: {
+                result: 'files are uploaded'
+            }
+        };
+    }
+});
